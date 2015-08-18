6d122926f95556a8f27ff797650468365e35bafe
Jim Staub <jamesstaub@Jamess-MBP.home>
2015-08-18 17:56:28 -0400

    logging

--- client/app/directives.js
+++ client/app/directives.js
@@ -1,7 +1,7 @@
 app.directive("jsCompile", function($compile) {
   return {
     scope: {
-        template: '=',
+        template: '=template',
     },
     link: function(scope, element) {
       console.log("template");
